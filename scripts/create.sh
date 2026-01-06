ed95a3f19885b22c63d3eb490b952fe96a7fb031
Corentin Tanguy <corentin.tanguy2@etu.univ-nantes.fr>
2026-01-06 14:51:14 +0100

    Correction scripts pour les ordis perso + ajout script setup codeigniter

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,1 +1,3 @@
-podman login docker.io && podman compose up --detach
+echo "IMPORTANT: Si la commande ne marche pas, et que vous êtes sur les PC de l'IUT, enelvez le - entre podman et compose"
+echo "Si vous êtes chez vous, remplacez 'podman compose' par 'podman-compose' (avec le - entre les 2)"
+podman login docker.io && podman-compose up --detach
--- scripts/down.sh
+++ scripts/down.sh
@@ -1,2 +1,4 @@
-podman compose down 
+echo "IMPORTANT: Si la commande ne marche pas, et que vous êtes sur les PC de l'IUT, enelvez le - entre podman et compose"
+echo "Si vous êtes chez vous, remplacez 'podman compose' par 'podman-compose' (avec le - entre les 2)"
+podman-compose down
 podman logout docker.io
